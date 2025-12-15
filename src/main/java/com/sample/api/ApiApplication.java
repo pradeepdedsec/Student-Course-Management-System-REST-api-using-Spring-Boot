fbd52b761fa898c7e876a44d0760487ffe63ccc0
Pradeep <pradeep@cloudrevelinnovation.com>
2025-12-15 13:17:59 +0530

    Replace property values with env variables

--- src/main/java/com/sample/api/ApiApplication.java
+++ src/main/java/com/sample/api/ApiApplication.java
@@ -1,7 +1,6 @@
 package com.sample.api;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.sample.api", "com.sample.api.repo"})
 @EnableJpaRepositories(basePackages = "com.sample.api.repo")
-public class ApiApplication implements CommandLineRunner {
+public class ApiApplication {
 
 
     public ApiApplication() {}
@@ -19,8 +18,4 @@ public class ApiApplication implements CommandLineRunner {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        //studentService.insertData();
-    }
 }
